99dc2803ce3cca83de38056a20cda7f701550910
CodeBlue0001 <145474893+CodeBlue0001@users.noreply.github.com>
2025-06-08 16:19:27 +0530

    Update Election_Result_server.js

--- Election_Result_server.js
+++ Election_Result_server.js
@@ -26,6 +26,8 @@ app.use('/uploads', express.static(uploadsDir));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Mongoose Schema and Model
 const VoteSchema = new mongoose.Schema({
   party: String
